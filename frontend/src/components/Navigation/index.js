b03e6d8ff31f277d4eca805830bc3b94e59e7b09
Andrea Cano <andreacanogisbert@gmail.com>
2023-01-02 19:31:31 -0800

    add navigation component

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+import ProfileButton from './ProfileButton';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import React from 'react';
+import './Navigation.css';
+
+
+const Navigation = ({ isLoaded }) => {
+  const sessionUser = useSelector(state => state.session.user);
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
