5c759e9a67c40353d14115c92e20b3e5732c4885
Andrea Cano <andreacanogisbert@gmail.com>
2023-03-21 11:01:43 -0700

    delete commented code

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -31,7 +31,6 @@ export const getUser = (userId) => (state) => {
 }
 
 
-
 export const fetchUsers = (type) => async dispatch => {
     const res = await csrfFetch(`/api/users?type=${type}`);
     if(res.ok) {
